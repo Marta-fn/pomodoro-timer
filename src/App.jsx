80a166457d108f3e7e4dbef025123c52f4d37500
Marta <160023329+Marta-fn@users.noreply.github.com>
2025-03-02 10:01:43 +0000

    Created basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <div className="bg-red-500 mx-auto">
-        <h1 className="text-6xl">Pomodoro Timer</h1>
-      </div>
+      <header>
+        <a href="">image/icon</a>
+        <h1>Tittle</h1>
+      </header>
+      <main>
+        <div>Focus/Pause/Long Pause</div>
+        <div>Time</div>
+        <div>Control buttons</div>
+      </main>
+      <footer>Project info</footer>
     </div>
   );
 };
