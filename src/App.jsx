702cf279694b51e6bceed211d6ff421383da659b
Marta <160023329+Marta-fn@users.noreply.github.com>
2025-03-02 09:40:43 +0000

    Installed Vite, React and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="bg-red-500 mx-auto">
+        <h1 className="text-6xl">Pomodoro Timer</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
