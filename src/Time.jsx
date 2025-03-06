fb471d805d3f3fec9ca2cf85a19a17376735b6c4
Marta <160023329+Marta-fn@users.noreply.github.com>
2025-03-06 15:04:41 +0000

    Added different session type
    The timer will now start on the work session type, do a short break and then start again a work session. Once its completed 4 work session will do a long break session.

--- src/Time.jsx
+++ src/Time.jsx
@@ -2,6 +2,8 @@ import React, { useState, useRef, useEffect } from "react";
 
 const Time = ({ playing }) => {
   const [timeLeft, settimeLeft] = useState(1500);
+  const [session, setsession] = useState("work");
+  const [workNum, setworkNum] = useState(1);
   const intervalRef = useRef(null);
 
   function startTimer() {
@@ -10,6 +12,12 @@ const Time = ({ playing }) => {
         if (prevTimeLeft <= 0) {
           clearInterval(intervalRef.current);
           intervalRef.current = null;
+          if (session === "work") {
+            setworkNum(workNum + 1);
+            setsession(workNum === 4 ? "long" : "short");
+          } else if ((session === "short") | (session === "long")) {
+            setsession("work");
+          }
           return 0;
         }
         return prevTimeLeft - 1;
@@ -32,6 +40,20 @@ const Time = ({ playing }) => {
       pauseTimer();
     }
   }, [playing]);
+
+  useEffect(() => {
+    if (session === "work" && playing) {
+      settimeLeft(1500);
+      startTimer();
+    } else if (session === "short") {
+      settimeLeft(300);
+      startTimer();
+    } else if (session === "long") {
+      settimeLeft(900);
+      setworkNum(1);
+      startTimer();
+    }
+  }, [session]);
 
   return (
     <div className="text-8xl">
